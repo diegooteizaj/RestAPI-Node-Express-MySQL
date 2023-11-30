6899a71bd36076246c6a9c3010fadfc5f65487b4
diegooteizaj <diego.oteiza@gmail.com>
2023-11-29 22:23:20 -0300

    fix: controller, router, routes de tramos

--- src/router.js
+++ src/router.js
@@ -1,7 +1,9 @@
 const customerRoutes = require('./routes/customers.routes');
+const tramosRoutes = require('./routes/tramos.routes')
 
 const router = (app) => {
     app.use('/personas', customerRoutes);
+    app.use('/tramos',tramosRoutes);
 };
 
 module.exports = router;
---
+++ src/routes/tramos.routes.js
@@ -0,0 +1,24 @@
+//* Import express and initialize the routers
+const express = require('express');
+const router = express.Router();
+
+//* Call the controller with the methods
+const {
+    getTramos,
+    // getPersonasById,
+    // createNewCustomer,
+    // updatePersona,
+    // deleteUnaPersona,
+    // createNewPersona,
+    // deleteAllCustomers
+  } = require('../controllers/tramosController');
+  
+//* Here I defined the methods 
+router.get('/', getTramos); //localhost:5000/customers/
+// router.get('/getIdPersona/:id', getPersonasById); //localhost:5000/customers/id/1
+// router.post('/addPersona', createNewPersona); //localhost:5000/customers/add
+// router.put('/updatePersona/:id', updatePersona); //localhost:5000/customers/edit/1
+// router.delete('/deletePersonas/:id', deleteUnaPersona); //localhost:5000/customers/delete/1
+// router.delete('/deleteCustomers', deleteAllCustomers); //localhost:5000/customers/deleteCustomers
+
+module.exports = router;
