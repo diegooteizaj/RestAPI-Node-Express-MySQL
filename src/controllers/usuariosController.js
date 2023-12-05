2f3984e5e6a0264b973d5b69de38b782ac621227
diegooteizaj <diego.oteiza@gmail.com>
2023-12-04 23:21:38 -0300

    Correccion para agregar id_rol en consulta de usuarios

--- src/controllers/usuariosController.js
+++ src/controllers/usuariosController.js
@@ -14,29 +14,32 @@ const getUsuarios = (req, res) => {
 // app.use(express.json());
 
 const obtenerUsuario = (req, res) => {
-  const { nombreUsuario, password } = req.body;
-
-  let sqlQuery = `
-    CALL ObtenerUsuario(?, ?, @codigoError, @codigoOk);
-    SELECT @codigoError AS codigoError, @codigoOk AS codigoOk;
-  `;
-  dbConnection.query(sqlQuery, [nombreUsuario, password], (error, results) => {
-    if (error) throw error;
-
-    const [result, codes] = results;
-
-    const { codigoError, codigoOk } = codes[0];
-
-    console.log('Código de error:', codigoError);
-    console.log('Código de éxito:', codigoOk);
-
-    if (codigoError) {
-      res.status(400).json({ error: 'Usuario no encontrado' });
-    } else {
-      res.status(200).json(codes[0]);
-    }
-  });
-};
+    const { nombreUsuario, password } = req.body;
+  
+    let sqlQuery = `
+      CALL ObtenerUsuario(?, ?, @codigoError, @codigoOk, @idRol);
+      SELECT @codigoError AS codigoError, @codigoOk AS codigoOk, @idRol AS idRol;
+    `;
+  
+    dbConnection.query(sqlQuery, [nombreUsuario, password], (error, results) => {
+      if (error) throw error;
+  
+      const [result, codes] = results;
+  
+      const { codigoError, codigoOk, idRol } = codes[0];
+  
+      console.log('Código de error:', codigoError);
+      console.log('Código de éxito:', codigoOk);
+      console.log('ID de Rol:', idRol);
+  
+      if (codigoError) {
+        res.status(400).json({ error: 'Usuario no encontrado' });
+      } else {
+        res.status(200).json({ codigoError, codigoOk, idRol });
+      }
+    });
+  };
+  
 
 
 
