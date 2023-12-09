0ec795cbf77dbc14b93af435ed83eb6d05822559
diegooteizaj <diego.oteiza@gmail.com>
2023-12-09 11:07:56 -0300

    se crea api de poryectos

--- src/router.js
+++ src/router.js
@@ -2,12 +2,15 @@ const customerRoutes = require('./routes/customers.routes');
 const tramosRoutes = require('./routes/tramos.routes');
 const anillosRoutes = require('./routes/anillos.routes');
 const usuariosRoutes = require('./routes/usuarios.routes');
+const proyectoRoutes = require('./routes/proyecto.routes');
+
 
 const router = (app) => {
     app.use('/personas', customerRoutes);
     app.use('/tramos',tramosRoutes);
     app.use('/anillos',anillosRoutes);
     app.use('/usuarios',usuariosRoutes);
+    app.use('/proyecto',proyectoRoutes);
 };
 
 module.exports = router;
